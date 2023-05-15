79858270852d4372def0e85e7f8a0d63440f046f
Petr Kim <ppeximo@gmail.com>
2023-05-15 11:27:35 +0300

    code reformat

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,8 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-       public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
@@ -25,7 +26,8 @@ public class Main {
         HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
         CloseableHttpResponse response = httpClient.execute(request);
         List<Cats> cats = mapper.readValue(response.getEntity().getContent(),
-                new TypeReference<>() {});
+                new TypeReference<>() {
+                });
         cats.stream().filter(value -> value.getUpvotes() != null && Integer.parseInt(value.getUpvotes()) > 0)
                 .forEach(System.out::println);
     }
